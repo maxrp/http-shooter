3abe5d72e0792fb88970201fdf1fb5a1e4ce659c
Maximus R.D. Parmer <maxp@pdx.edu>
2015-02-23 11:55:15 -0800

    Handles alternate ports in shoot-page.js

--- shoot-page.js
+++ shoot-page.js
@@ -1,14 +1,35 @@
 var system = require('system');
 var page = require('webpage').create();
 
-if (system.args.length === 1) {
-    console.log('Usage: phantomjs shoot-page.js http://example.com');
+if (system.args.length !== 2) {
+    console.log('Usage: phantomjs shoot-page.js example.com:80');
+    phantom.exit();
 } else {
-    var target = system.args[1]
-    var filename = system.args[1] + '.png'
-    console.log('Capturing: ' + target);
-    page.open("http://" + target, function() {
-        page.render(filename);
+    var target = system.args[1].split(":")[0]
+    var port = system.args[1].split(":")[1];
+    var port = port? port : 80;
+    var filename = target + ':' + port + '.png';
+
+    switch (port) {
+        case '80':
+        case '8000':
+        case '8080':
+            protocol = 'http://';
+            break;
+        case '443':
+        case '8443':
+            protocol = 'https://';
+            break;
+    }
+    var url = protocol + target + ":" + port;
+
+    page.open(url, function(status) {
+        if (status !== 'success') {
+            console.log("Failed to capture: " + url);
+        } else {
+            page.render(filename);
+            console.log('Captured: ' + url);
+        }
         phantom.exit();
     });
 }
